50ae69574e4b1700113c064f219d8809a31d85c2
Patinyaporn Sudtho <cosyprang@gmail.com>
2025-02-10 03:55:35 +0700

    homepage finished with no route

--- script.js
+++
@@ -1,9 +0,0 @@
-function gotoHome(){
-    window.location.href = "homePage.html";
-}
-function gotoAbout(){
-    window.location.href = "aboutPage.html";
-}
-function goBack(){
-    window.location.href = document.referrer;
-}
