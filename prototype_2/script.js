a5e7455065018999dad1917f64b3291429040ea7
Patinyaporn Sudtho <cosyprang@gmail.com>
2025-02-15 22:06:50 +0700

    fetch json data using api and display in html div

--- prototype_2/script.js
+++ prototype_2/script.js
@@ -1,3 +1,31 @@
+const dataContainer = document.getElementById('user-data');
+
+fetch ('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => {
+            console.log(post);
+            const postElement = document.createElement("p");
+            postElement.innerHTML = `
+                Post ID: ${post.id} <br>
+                Name: ${post.name}  <br>
+                Email: ${post.email}
+            `;
+            dataContainer.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error('Error fetching data', error));
+
+
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('show');
 })
+    
+// ****************** Function linktoPage ****************** //
+function gotoTeam(){
+    window.location.href = './teamPage';
+}
+
+function goBack(){
+    window.history.back();
+}
